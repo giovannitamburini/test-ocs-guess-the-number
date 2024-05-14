d5d1772b1283fd1ccaf92c3eb1c02a907bb9c645
giovannitamburini <giovatambu@gmail.com>
2024-05-14 12:34:43 +0200

    feat: added button and click event to generate random number

--- js/script.js
+++ js/script.js
@@ -3,12 +3,20 @@ let winningNumber;
 
 
 
+// btn per entrare nel gioco
+let btnStart = document.getElementById('btn-start');
 
 
 
 
+// evento click al bottone per dare via al gioco
+btnStart.addEventListener("click", function(){
 
+    // salvo il numero casuale
+    winningNumber = generateRandomNumber(0, 100);
 
+    console.log(winningNumber);
+})
 
 
 
